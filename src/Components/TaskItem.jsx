e39c617de849c8e82f1760dcbdeeec049cc8831a
kratiDatacode <krati.datacode@gmail.com>
2025-07-26 19:49:02 +0530

    Task krati : add undo ,done function

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -1,10 +1,14 @@
+import { useDispatch } from "react-redux";
+import { deleteTask, toggleTask } from "../features/task/taskSlice";
+
 const TaskItem = ({task}) =>{
+    const dispatch = useDispatch();
         return(
             <li>
                 {task.text}
                 <div>
-                    <button>Done</button>
-                    <button>Delete</button>
+                    <button onClick={()=>dispatch(toggleTask(task.id))}>{task.completed ? "Undo" : "Done" }</button>
+                    <button onClick={()=> dispatch(deleteTask(task.id))}>Delete</button>
                 </div>
             </li>
         )
